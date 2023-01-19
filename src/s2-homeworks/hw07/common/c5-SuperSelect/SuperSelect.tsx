e98be6d82d16bfee947f8bf29d9a6731437b950a
Aleksandr_UB <bogdanov777000@mail.ru>
2023-01-19 14:03:53 +0300

    HW7 - готово

--- src/s2-homeworks/hw07/common/c5-SuperSelect/SuperSelect.tsx
+++ src/s2-homeworks/hw07/common/c5-SuperSelect/SuperSelect.tsx
@@ -4,39 +4,48 @@ import React, {
     ChangeEvent,
 } from 'react'
 import s from './SuperSelect.module.css'
-
+import {OptionType} from "../../HW7";
+// залазим внутрь DetailedHTMLProps и смотрим дефолтные пропсы - их уточняем атрибутами select
 type DefaultSelectPropsType = DetailedHTMLProps<
     SelectHTMLAttributes<HTMLSelectElement>,
     HTMLSelectElement
 >
 
+
 type SuperSelectPropsType = DefaultSelectPropsType & {
-    options?: any[]
+    options?: OptionType[]
     onChangeOption?: (option: any) => void
 }
 
 const SuperSelect: React.FC<SuperSelectPropsType> = ({
     options,
+// className - явл-тся дефолтным пропсом
+// но т.к он нам нужен в этом файле его тоже деструктуризируем - иначе он попал бы в {...restProps}
     className,
+    // onChange - явл-тся дефолтным пропсом но т.к он нам нужен в этом файле его тоже деструктуризируем
+// - иначе он попал бы в {...restProps}
     onChange,
     onChangeOption,
     ...restProps
 }) => {
+
     const mappedOptions: any[] = options
-        ? options.map((o) => (
+        ? options.map((option) => (
               <option
-                  id={'hw7-option-' + o.id}
+                  id={'hw7-option-' + option.id}
                   className={s.option}
-                  key={o.id}
-                  value={o.id}
+                  key={option.id}
+                  value={option.id}
               >
-                  {o.value}
+                  {option.value}
               </option>
           ))
         : [] // map options with key
 
     const onChangeCallback = (e: ChangeEvent<HTMLSelectElement>) => {
         // делают студенты
+        onChange && onChange(e)
+        onChangeOption?.(e.currentTarget.value)
     }
 
     const finalSelectClassName = s.select + (className ? ' ' + className : '')
